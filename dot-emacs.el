e9d0e5848e31a4ce458a717f3da6be0bed891f87
magnus <magnus@genesilico.pl>
2017-02-28 23:11:55 +0100

    update custom-set-variables

--- dot-emacs.el
+++ dot-emacs.el
@@ -174,89 +174,91 @@
 
   (add-hook 'html-helper-mode-hook 'my-html-mode-hook)
 ;; SETTINGS -----------------------------------------------------------------
-  (custom-set-variables
-   ;; custom-set-variables was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(bmkp-last-as-first-bookmark-file "/home/magnus/.emacs.d/bookmarks")
-   '(column-number-mode t)
-   '(custom-enabled-themes (quote (tsdh-light)))
-   '(display-time-mode t)
-   '(fringe-mode (quote (0)) nil (fringe))
-   '(markdown-hr-string
-     "--------------------------------------------------------------------------------")
-   '(org-agenda-category-icon-alist
-     (quote
-      (("work-curr" "~/Dropbox/curr_dropbox/org_mode_icon/icon.png" nil nil))))
-   '(org-agenda-custom-commands
-     (quote
-      (("o" "Agenda and all TODO's work*.org"
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/work-curr.org")))))
-         (alltodo ""
-  		((org-agenda-files
-  		  (quote
-  		   ("~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-inbox.org")))))))
-       ("l" "Agenda life*.org"
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))))
-       (";" "Agenda and all TODO's life*.org"
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))
-         (alltodo ""
-  		((org-agenda-files
-  		  (quote
-  		   ("~/Dropbox/geekbook/notes/life-archive.org" "~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))))
-       ("x" "Agenda and all TODO's eXtremal science "
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/science.org")))))
-         (alltodo ""
-  		((org-agenda-files
-  		  (quote
-  		   ("~/Dropbox/geekbook/notes/science.org")))))))
-       ("k" "Agenda and all TODO's s[k]ills"
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/skills-curr.org")))))
-         (alltodo ""
-  		((org-agenda-files
-  		  (quote
-  		   ("~/Dropbox/geekbook/notes/skills-curr.org")))))))
-       ("w" "Agenda work*.org"
-        ((agenda ""
-  	       ((org-agenda-files
-  		 (quote
-  		  ("~/Dropbox/geekbook/notes/work-someday.org" "~/Dropbox/geekbook/notes/work-extra.org" "~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-archive.org" "~/Dropbox/geekbook/notes/work-inbox.org")))))))
-       ("n" "Agenda and all TODO's"
-        ((agenda "" nil)
-         (alltodo "" nil))
-        nil))))
-   '(org-agenda-files
-     (quote
-      ("~/Dropbox/geekbook/notes/work-someday.org" "~/Dropbox/geekbook/notes/work-extra.org" "~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-archive.org" "~/Dropbox/geekbook/notes/work-inbox.org")))
-   '(org-agenda-span (quote day))
-   '(org-indent-indentation-per-level 5)
-   '(org-indent-mode-turns-on-hiding-stars f)
-   '(package-selected-packages
-     (quote
-      (pylint python jedi-core ipython outline-magic writeroom-mode wanderlust tidy synonyms stem skype python-pylint python-pep8 python-mode projectile powerline multi-term markdown-mode+ magit-tramp jedi jabber hipster-theme helm-ispell helm google-translate git-rebase-mode git-commit-mode focus flyspell-popup flymake-python-pyflakes flymake flycheck fiplr find-file-in-repository exec-path-from-shell ess-smart-underscore ess-R-object-popup eimp ecb dictionary darkroom color-theme cl-generic calfw-gcal calfw auto-yasnippet auto-dictionary ac-slime ac-python ac-php-core ac-ispell ac-R)))
-   '(tool-bar-mode nil))
-  (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   )
+  
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/home/magnus/.emacs.d/bookmarks")
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tsdh-light)))
+ '(display-time-mode t)
+ '(fringe-mode (quote (0)) nil (fringe))
+ '(markdown-hr-string
+   "--------------------------------------------------------------------------------")
+ '(org-agenda-category-icon-alist
+   (quote
+    (("work-curr" "~/Dropbox/curr_dropbox/org_mode_icon/icon.png" nil nil))))
+ '(org-agenda-custom-commands
+   (quote
+    (("o" "Agenda and all TODO's work*.org"
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/work-curr.org")))))
+       (alltodo ""
+		((org-agenda-files
+		  (quote
+		   ("~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-inbox.org")))))))
+     ("l" "Agenda life*.org"
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))))
+     (";" "Agenda and all TODO's life*.org"
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))
+       (alltodo ""
+		((org-agenda-files
+		  (quote
+		   ("~/Dropbox/geekbook/notes/life-archive.org" "~/Dropbox/geekbook/notes/life-curr.org" "~/Dropbox/geekbook/notes/life-today.org")))))))
+     ("x" "Agenda and all TODO's eXtremal science "
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/science.org")))))
+       (alltodo ""
+		((org-agenda-files
+		  (quote
+		   ("~/Dropbox/geekbook/notes/science.org")))))))
+     ("k" "Agenda and all TODO's s[k]ills"
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/skills-curr.org")))))
+       (alltodo ""
+		((org-agenda-files
+		  (quote
+		   ("~/Dropbox/geekbook/notes/skills-curr.org")))))))
+     ("w" "Agenda work*.org"
+      ((agenda ""
+	       ((org-agenda-files
+		 (quote
+		  ("~/Dropbox/geekbook/notes/work-someday.org" "~/Dropbox/geekbook/notes/work-extra.org" "~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-archive.org" "~/Dropbox/geekbook/notes/work-inbox.org")))))))
+     ("n" "Agenda and all TODO's"
+      ((agenda "" nil)
+       (alltodo "" nil))
+      nil))))
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/geekbook/notes/work-someday.org" "~/Dropbox/geekbook/notes/work-extra.org" "~/Dropbox/geekbook/notes/work-curr.org" "~/Dropbox/geekbook/notes/work-archive.org" "~/Dropbox/geekbook/notes/work-inbox.org")))
+ '(org-agenda-span (quote day))
+ '(org-indent-indentation-per-level 5)
+ '(org-indent-mode-turns-on-hiding-stars f)
+ '(package-selected-packages
+   (quote
+    (ac-helm pylint python jedi-core ipython outline-magic writeroom-mode wanderlust tidy synonyms stem skype python-pylint python-pep8 python-mode projectile powerline multi-term markdown-mode+ magit-tramp jedi jabber hipster-theme helm-ispell helm google-translate git-rebase-mode git-commit-mode focus flyspell-popup flymake-python-pyflakes flymake flycheck fiplr find-file-in-repository exec-path-from-shell ess-smart-underscore ess-R-object-popup eimp ecb dictionary darkroom color-theme cl-generic calfw-gcal calfw auto-yasnippet auto-dictionary ac-slime ac-python ac-php-core ac-ispell ac-R)))
+ '(tool-bar-mode nil))
+  
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 110 :width normal)))))
 ;; My plugins ----------------------------------------------------------
   ;; Insert curr date use with Geekbook
   ;; http://www.emacswiki.org/emacs/InsertingTodaysDate
