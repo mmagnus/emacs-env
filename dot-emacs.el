0261ca33ffd73202fb03435f53b26a3acd78036d
mmagnus <m.magnus@zoho.com>
2017-07-13 16:41:12 +0200

    [old] insert-current-date

--- dot-emacs.el
+++ dot-emacs.el
@@ -484,3 +484,13 @@
 ;; tramp
 (require 'tramp)
 (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+
+;; My plugins ----------------------------------------------------------
+  ;; Insert curr date use with Geekbook
+  ;; http://www.emacswiki.org/emacs/InsertingTodaysDate
+  (defun insert-current-date () (interactive)
+     (insert (shell-command-to-string "echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
+  (global-set-key (kbd "C-c .") 'insert-current-date)
+
+
