6a13f7590a7ecbb8896412b7e3e3c803b9088098
mmagnus <m.magnus@zoho.com>
2017-08-15 20:16:50 +0200

    --<date>

--- dot-emacs.el
+++ dot-emacs.el
@@ -667,7 +667,7 @@
   ;; Insert curr date use with Geekbook
   ;; http://www.emacswiki.org/emacs/InsertingTodaysDate
   (defun insert-current-date () (interactive)
-     (insert (shell-command-to-string "echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
+     (insert (shell-command-to-string "echo -n '--' && echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
   (global-set-key (kbd "C-c .") 'insert-current-date)
 
 
