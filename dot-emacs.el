02553373fcf730693f55100a898721526ff8c3f2
mmagnus <m.magnus@zoho.com>
2017-08-18 20:07:23 +0200

    hide-the-backup-files-that-emacs-creates

--- dot-emacs.el
+++ dot-emacs.el
@@ -250,5 +250,21 @@
 ;; @writing
 (load "~/.emacs.d/my-python.el")
 
+
+;; https://stackoverflow.com/questions/2020941/how-can-i-hide-the-backup-files-that-emacs-creates
+;; Don't clutter up directories with files~
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+;(setq dotfiles-dir (file-name-directory (or load-file-name (buffer-file-name))))
+;(setq backup-directory-alist `(("." . ,(expand-file-name
+ ;                                   (concat dotfiles-dir "backups")))))
+
+;; Don't clutter with #files either
+;(setq auto-save-file-name-transforms
+;      `((".*" ,(expand-file-name (concat dotfiles-dir "backups")))))
+
+
 ;; https://www.emacswiki.org/emacs/NoTabs
 (setq-default indent-tabs-mode nil)
