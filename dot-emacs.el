0e7e3b66ac575350fd5082fd015204d395d3d2cb
mmagnus <m.magnus@zoho.com>
2017-12-01 22:54:30 +0100

    dump

--- dot-emacs.el
+++ dot-emacs.el
@@ -21,13 +21,14 @@
 
 ;; Emacs Lisp Package Archive
 (require 'package)
-(add-to-list 'package-archives
-             '("elpa" . "http://tromey.com/elpa/"))
 ;; Add the user-contributed repository
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
        '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 
@@ -278,8 +279,6 @@
  '(ac-ispell-requires 4)
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#000000" "#d01A4E" "#7E7D7E" "#b58900" "#268bd2" "#d33682" "#2aa198" "#DCDCCC"])
  '(bmkp-last-as-first-bookmark-file "/home/magnus/.emacs.d/bookmarks")
  '(column-number-mode t)
  '(csv-separators (quote (",")))
@@ -288,17 +287,14 @@
     ("eea01f540a0f3bc7c755410ea146943688c4e29bea74a29568635670ab22f9bc" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "ed5af4af1d148dc4e0e79e4215c85e7ed21488d63303ddde27880ea91112b07e" "9a2dcb3d7c42d508d5bb78eef98c8e9a71ec4ef8bd88a6677e3c237c73fa20eb" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "3b5ce826b9c9f455b7c4c8bff22c020779383a12f2f57bf2eb25139244bb7290" default)))
  '(display-battery-mode t)
  '(display-time-mode t)
- '(fci-rule-color "#383838")
  '(indicate-empty-lines t)
  '(jedi:tooltip-method nil t)
  '(linum-format " %5i ")
  '(magit-git-executable "git")
  '(markdown-fontify-code-blocks-natively t)
  '(markdown-hr-string "****************************************************")
+ '(markdown-max-image-size (quote (600 . 400)))
  '(menu-bar-mode nil)
- '(nrepl-message-colors
-   (quote
-    ("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896")))
  '(org-agenda-custom-commands
    (quote
     (("o" "Agenda and all TODO's work*.org"
@@ -359,16 +355,17 @@
  '(org-indent-mode-turns-on-hiding-stars f)
  '(package-selected-packages
    (quote
-    (paredit org-autolist load-theme-buffer-local flycheck-inline org-download monokai-alt-theme sublime-themes realgud python-docstring flyspell-correct-popup flyspell-lazy dic-lookup-w3m build-status flycheck-color-mode-line flymd flycheck-pyflakes django-mode web-narrow-mode web-mode jedi github-theme color-theme-buffer-local uimage csv-mode w3m org-gcal darkroom google-this langtool org-random-todo emojify el-pocket blank-mode ido-vertical-mode ox-gfm auto-org-md sphinx-mode sphinx-frontend sphinx-doc auto-complete-rst ac-helm python ipython outline-magic writeroom-mode wanderlust tidy synonyms stem skype python-pylint python-pep8 python-mode projectile powerline multi-term markdown-mode+ magit-tramp jabber hipster-theme helm-ispell helm google-translate git-rebase-mode git-commit-mode focus flyspell-popup flymake-python-pyflakes flymake flycheck fiplr exec-path-from-shell ess-smart-underscore ess-R-object-popup eimp ecb dictionary color-theme cl-generic calfw-gcal calfw auto-yasnippet auto-dictionary ac-slime ac-python ac-php-core ac-ispell ac-R)))
- '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
- '(py-keep-windows-configuration t)
+    (markdown-preview-mode markdown-mode paredit org-autolist load-theme-buffer-local flycheck-inline org-download monokai-alt-theme sublime-themes realgud python-docstring flyspell-correct-popup flyspell-lazy dic-lookup-w3m build-status flycheck-color-mode-line flymd flycheck-pyflakes django-mode web-narrow-mode web-mode jedi github-theme color-theme-buffer-local uimage csv-mode w3m org-gcal darkroom google-this langtool org-random-todo emojify el-pocket blank-mode ido-vertical-mode ox-gfm auto-org-md sphinx-mode sphinx-frontend sphinx-doc auto-complete-rst ac-helm python ipython outline-magic writeroom-mode wanderlust tidy synonyms stem skype python-pylint python-pep8 python-mode projectile powerline multi-term markdown-mode+ magit-tramp jabber hipster-theme helm-ispell helm google-translate git-rebase-mode git-commit-mode focus flyspell-popup flymake-python-pyflakes flymake flycheck fiplr exec-path-from-shell ess-smart-underscore ess-R-object-popup eimp ecb dictionary color-theme cl-generic calfw-gcal calfw auto-yasnippet auto-dictionary ac-slime ac-python ac-php-core ac-ispell ac-R)))
+ '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")) t)
+ '(py-keep-windows-configuration t t)
+ '(send-mail-function nil)
  '(show-paren-mode t)
  '(synonyms-cache-file
    "/Users/magnus/Dropbox/workspace/emacs-env/dot-emacs.d/synonimous/mthesaur.txt.cache")
  '(synonyms-file
    "/Users/magnus/Dropbox/workspace/emacs-env/dot-emacs.d/synonimous/mthesaur.txt")
  '(tool-bar-mode nil)
- '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-background "#2B2B2B" t)
  '(vc-annotate-color-map
    (quote
     ((20 . "#dc322f")
@@ -388,8 +385,8 @@
      (300 . "#268bd2")
      (320 . "#268bd2")
      (340 . "#00a74e")
-     (360 . "#d33682"))))
- '(vc-annotate-very-old-color "#d33682"))
+     (360 . "#d33682"))) t)
+ '(vc-annotate-very-old-color "#d33682" t))
 
 
 ;; https://www.emacswiki.org/emacs/AutoSave
