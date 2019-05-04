845ac59e7e15af4b04d513ba18e53842d6e4700e
Marcin Magnus <mag_dex@o2.pl>
2019-05-04 10:53:51 +0200

    Shortcut for pull-inbox

--- dot-emacs.d/my-plugins.el
+++ dot-emacs.d/my-plugins.el
@@ -5,3 +5,13 @@
      ;(insert (shell-command-to-string "echo -n '--' && echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
      (insert (shell-command-to-string "echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
   (global-set-key (kbd "C-c .") 'insert-current-date)
+
+(defun pull-inbox () (interactive)
+       (find-file-other-window "/home/magnus/Dropbox/geekbook/notes/sandbox.org")
+       (org-mobile-pull)
+       (org-mobile-push)
+       )
+
+(define-key global-map "\C-cI" 'pull-inbox)
+
+;;; package --- Summary
