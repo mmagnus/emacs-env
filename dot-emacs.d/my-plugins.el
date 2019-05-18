5cc8f7fa0732ca1d3330a7db5c3cf4a472e22be6
Marcin Magnus <mag_dex@o2.pl>
2019-05-18 17:49:41 +0200

    pull-inbox: fix path to iCloud

--- dot-emacs.d/my-plugins.el
+++ dot-emacs.d/my-plugins.el
@@ -1,13 +1,13 @@
-;; My plugins ----------------------------------------------------------
+;; My plugnins ----------------------------------------------------------
   ;; Insert curr date use with Geekbook
   ;; http://www.emacswiki.org/emacs/InsertingTodaysDate
   (defun insert-current-date () (interactive)
      ;(insert (shell-command-to-string "echo -n '--' && echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
-     (insert (shell-command-to-string "echo -n $(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
+     (insert (shell-command-to-string "echo -n ''$(date +%y%m%d)"))) ;; echo -n $(date +%y%m%d)
   (global-set-key (kbd "C-c .") 'insert-current-date)
 
 (defun pull-inbox () (interactive)
-       (find-file-other-window "/home/magnus/Dropbox/geekbook/notes/sandbox.org")
+       (find-file-other-window "/home/magnus/iCloud/geekbook/notes/sandbox.org")
        (org-mobile-pull)
        (org-mobile-push)
        )
