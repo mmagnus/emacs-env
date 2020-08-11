285de6b0bebeaaac811840e073aa51b0926f81e4
Marcin Magnus <mag_dex@o2.pl>
2020-08-11 13:40:35 +0200

    Control the compression shell command for `dired-do-compress-to

--- dot-emacs.el
+++ dot-emacs.el
@@ -909,6 +909,20 @@ want to use in the modeline *in lieu of* the original.")
 ;;; Shut up compile saves
 (setq compilation-ask-about-save nil)
 
+
+;; compression https://oremacs.com/2015/10/23/dired-compress/
+(defvar dired-compress-files-alist
+  '(("\\.tar\\.gz\\'" . "tar -c %i | gzip -c9 > %o")
+    ("\\.zip\\'" . "zip %o -r --filesync %i"))
+  "Control the compression shell command for `dired-do-compress-to'.
+
+Each element is (REGEXP . CMD), where REGEXP is the name of the
+archive to which you want to compress, and CMD the the
+corresponding command.
+
+Within CMD, %i denotes the input file(s), and %o denotes the
+output file. %i path(s) are relative, while %o is absolute.")
+
 ;(require 'grammarly)
 ;(add-hook 'markdown-mode-hook 'flyspell-grammarly)
 ;;;;;;;;;
