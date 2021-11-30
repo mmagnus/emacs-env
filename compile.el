1cde742577b86b5fc3ab9d0da2f580c957c9cfb7
Marcin Magnus <mag_dex@o2.pl>
2021-11-30 11:27:29 +0100

    compile: up

--- compile.el
+++ compile.el
@@ -1,5 +1,6 @@
 ;;; Shut up compile saves
 (setq compilation-ask-about-save nil)
-(setq compile-command "./test.sh")
+(setq compile-command "chmod +x test.sh && ./test.sh")
 ;; recompile shorthut
 (global-set-key "\C-cc" 'recompile)
+(global-set-key "\C-ce" 'compile)
