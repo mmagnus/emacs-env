2f92757f92ec9153534f243c5b6a755c631b2141
mmagnus <mag_dex@o2.pl>
2023-12-07 11:18:24 -0500

    org-ai-mode

--- dot-emacs.el
+++ dot-emacs.el
@@ -291,3 +291,19 @@
 ;; also for .el231207
 (add-to-list 'auto-mode-alist '("\\.el" . emacs-lisp-mode))
 
+;;
+(use-package org-ai
+  :ensure t
+  :commands (org-ai-mode
+             org-ai-global-mode)
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode) ; enable org-ai in org-mode
+  (org-ai-global-mode) ; installs global keybindings on C-c M-a
+  :config
+  (setq org-ai-default-chat-model "gpt-4") ; if you are on the gpt-4 beta:
+  (org-ai-install-yasnippets)) ; if you are using yasnippet and want `ai` snippets
+
+(global-set-key (kbd "C-c q m") 'org-ai-summarize)
+(load-file "~/.emacs.d/tokens.el")
+
+(load-file "~/workspace/emacs-send-to-chatgpt/emacs-send-to-chatgpt.el")
